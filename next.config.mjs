393942703e02864ebfb451064b9e869b2e6b75eb
prasadsawant7 <prasadsawant7git@gmail.com>
2024-07-13 01:07:03 +0530

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+import { fileURLToPath } from "node:url";
+import createJiti from "jiti";
+const jiti = createJiti(fileURLToPath(import.meta.url));
+
+jiti("./src/env/client.ts");
+jiti("./src/env/server.ts");
+
 const nextConfig = {};
 
 export default nextConfig;
---
+++ src/env/server.ts
@@ -0,0 +1,13 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z.string().url(),
+    OPEN_AI_API_KEY: z.string().min(1),
+  },
+  runtimeEnv: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    OPEN_AI_API_KEY: process.env.OPEN_AI_API_KEY,
+  },
+});
