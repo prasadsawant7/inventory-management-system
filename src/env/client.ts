e5e63b2c94b310226582b7c52f72919501151077
prasadsawant7 <prasadsawant7git@gmail.com>
2024-07-13 16:45:14 +0530

    cicd: trying cicd pipeline on dev

--- src/env/client.ts
+++ src/env/client.ts
@@ -3,10 +3,9 @@ import { z } from "zod";
 
 export const env = createEnv({
   client: {
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
+    // NEXT_PUBLIC_VERCEL_ENV: z.enum(["production", "preview", "development"]),
   },
   runtimeEnv: {
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
-      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    // NEXT_PUBLIC_VERCEL_ENV: process.env.NEXT_PUBLIC_VERCEL_ENV,
   },
 });
