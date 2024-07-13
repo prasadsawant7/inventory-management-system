1984da27da545eeae5623c4f5eae10d4f730768e
prasadsawant7 <prasadsawant7git@gmail.com>
2024-07-13 16:56:43 +0530

    env: added a env variable for unpooled db url

--- src/env/server.ts
+++ src/env/server.ts
@@ -3,10 +3,12 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
+    NEON_DATABASE_URL_UNPOOLED: z.string().url(),
     NEON_DATABASE_URL: z.string().url(),
     NODE_ENV: z.enum(["development", "production", "test"]),
   },
   runtimeEnv: {
+    NEON_DATABASE_URL_UNPOOLED: process.env.NEON_DATABASE_URL_UNPOOLED,
     NEON_DATABASE_URL: process.env.NEON_DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
   },
