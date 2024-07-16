6d952b4b10c89037187b1fcc8204d2d2f31b0f06
prasadsawant7 <prasadsawant7git@gmail.com>
2024-07-16 20:05:50 +0530

    Created Customer layout

---
+++ src/app/customer/layout.tsx
@@ -0,0 +1,15 @@
+import Sidebar from "@/components/sidebar";
+import Topbar from "@/components/topbar";
+
+export default function CustomerLayout({ children }: RootLayoutProps) {
+  return (
+    <div className="grid min-h-screen w-full">
+      <div className="flex flex-col">
+        <Topbar />
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+}
